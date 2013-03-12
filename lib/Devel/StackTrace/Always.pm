3a2e5f14fc4eb9420bbefc2df623b9c89e054c09
Eric Johnson <eric.git@iijo.org>
2013-03-12 14:04:21 +0000

    POD

--- lib/Devel/StackTrace/Always.pm
+++ lib/Devel/StackTrace/Always.pm
@@ -44,9 +44,66 @@ END {
   @SIG{qw(__DIE__ __WARN__)} = @OLD_SIG{qw(__DIE__ __WARN__)};
 }
 
-=pod
+=head1 SYNOPSIS
+
+    use Devel::StackTrace::Always ignore => [qw/sharks bears/];
+
+    wolves();
+
+    sub wolves { sharks()    };
+    sub sharks { bears()     };
+    sub bears  { lions()     };
+    sub lions  { snakes()    };
+    sub snakes { die('boop') };
+
+    # output looks like:
+    boop at predators.pl line 9.
+            main::snakes at predators.pl line 8
+            main::lions at predators.pl line 7
+            main::wolves at predators.pl line 3
 
 
+=head1 DESCRIPTION
+
+This module is inspired by Carp::Always.  It is meant as a debugging aid.   It
+forces a script to print a stacktrace when warn()ing or die()ing.  
+
+The advantage this module has over Carp::Always is that it allows the user to
+filter items out of the stacktrace.  This is useful in larger applications
+where stacktraces can get very long and where you often want to filter out
+parts of a stacktrace.  For example a Plack application usually doesn't need
+Plack to be part of the stacktrace.
+
+=head1 MORE IDEAS
+
+=over4
+
+=item Figure out how to ignore stuff via the command line
+
+=item Shorter module name (alias) for use on the command line
+
+=item A more readable default stacktrace output
+
+=item Configurable stacktrace output?
+
+=item Colored output?
+
+=item An easy way to toggle showing all stacktraces vs hiding ignored parts of the stack
+
+=item Plugins on CPAN which ignore parts of the stack from certain applications?
+
+For example Devel::StackTrace::Filter::Plack, Devel::StackTrace::Filter::Catalyst.  
+
+=item Configuration files?
+
+Allow setting configuration via a file.  Like Data::Printer does.
+
+=item Split out some code into Devel::StackTrace::Filter?
+
+Perhaps this should be 2 modules: Devel::StackTrace::Filter and
+Devel::StackTrace::Always.  
+
+=back
 
 =cut
 
